b8f792b7f8420e27aa17dc6adb0f5bbdbcbe231a
Thomas Wang <hi@xgwang.me>
2021-06-03 13:11:08 -0700

    Add loading spin

--- src/components/Report.js
+++ src/components/Report.js
@@ -4,26 +4,57 @@ import {
   NavLink,
   useParams,
   useRouteMatch,
-} from 'react-router-dom';
-import { HumanRaceChart } from './HumanRaceChart';
+} from "react-router-dom";
+import { HumanRaceChart } from "./HumanRaceChart";
 
 const tabs = [
-  { name: 'All', to: '' },
-  { name: 'Black', to: '/black' },
-  { name: 'AAPI', to: '/aapi' },
-  { name: 'Latino', to: '/latino' },
-  { name: 'White', to: '/white' },
-  { name: 'Other', to: '/other' },
+  { name: "All", to: "" },
+  { name: "Black", to: "/black" },
+  { name: "AAPI", to: "/aapi" },
+  { name: "Latino", to: "/latino" },
+  { name: "White", to: "/white" },
+  { name: "Other", to: "/other" },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
+}
+
+function LoadingSpin() {
+  return (
+    <div className="flex justify-around">
+      <span className="inline-flex">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </span>
+    </div>
+  );
 }
 
 export function Report({ diversityData }) {
   let { path, url } = useRouteMatch();
 
-  return (
+  return diversityData === null ? (
+    <LoadingSpin />
+  ) : (
     <div className="mx-8 mt-2">
       <h2 id="diversity-report-navigation" className="font-bold text-gray-800">
         Your following's diversity distribution
@@ -40,9 +71,9 @@ export function Report({ diversityData }) {
             to={`${url}${tab.to}`}
             activeClassName="bg-indigo-100 text-gray-900"
             className={classNames(
-              'text-gray-500',
-              'bg-gray-100',
-              'group relative min-w-0 flex-1 overflow-hidden rounded-full py-1 px-1 text-xs text-center focus:z-10'
+              "text-gray-500",
+              "bg-gray-100",
+              "group relative min-w-0 flex-1 overflow-hidden rounded-full py-1 px-1 text-xs text-center focus:z-10"
             )}
             aria-current="page"
           >
@@ -90,7 +121,7 @@ function RaceReport({ diversityData }) {
   const race = tabs.find((t) => t.to.includes(raceId))?.name;
   let races;
   if (!race) {
-    races = ['Black', 'AAPI', 'Latino', 'White', 'Other'];
+    races = ["Black", "AAPI", "Latino", "White", "Other"];
   } else {
     races = [race];
   }
