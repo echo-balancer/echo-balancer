1f98aac5455ed8be18d00f65b284ac9d01f29708
Thomas Wang <hi@xgwang.me>
2021-06-03 20:31:54 -0700

    Use 2 gunicorn workers

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --workers=1 app:app
+web: gunicorn --workers=2 app:app
--- src/components/LoadingSpin.js
+++ src/components/LoadingSpin.js
@@ -14,7 +14,7 @@ export function LoadingSpin() {
             cy="12"
             r="10"
             stroke="currentColor"
-            stroke-width="4"
+            strokeWidth="4"
           ></circle>
           <path
             className="opacity-75"
