441cabdc34adb024e973cffc50aaed978297c7b3
Thomas Wang <hi@xgwang.me>
2021-06-05 10:33:48 -0700

    Add GA4 (#44)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+
+function initializeGA() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-YXVZDLCTXW");
+}
+
+if (process.env.NODE_ENV === "production") {
+  initializeGA();
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
